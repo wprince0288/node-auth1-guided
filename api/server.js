0da43ae73a3dc7da3cfcb0e94729d9c999a66ab0
Walter Prince <wprince0288@gmail.com>
2025-06-28 15:11:54 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,18 +1,18 @@
 const path = require('path')
 const express = require('express')
-
+const authRouter = require('./auth/auth-router')
 const usersRouter = require('./users/users-router.js')
 
 const server = express()
 
 server.use(express.static(path.join(__dirname, '../client')))
 server.use(express.json())
-
+server.use('/api/auth', authRouter)
 server.use('/api/users', usersRouter)
 
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'index.html'))
-})
+}) 
 
 server.use('*', (req, res, next) => {
   next({ status: 404, message: 'not found!' })
