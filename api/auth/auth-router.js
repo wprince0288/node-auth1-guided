f05c90c925f97a37bcef835a2d38d6cb7a891ff3
Walter Prince <wprince0288@gmail.com>
2025-06-29 15:53:46 -0500

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,13 +1,37 @@
 const express = require('express')
-
+const bcryptjs = require('bcryptjs')
 const router = express.Router()
+const User = require('../users/users-model')
 
 router.post('/register', async (req, res, next) => {
-    res.json({ message: 'register working' })
+    try {
+        const { username, password } = req.body
+        const hash = bcrypt.hashSync(password, 8)
+        const newUser = { username, password: hash }
+        const result = await User.add(newUser)
+        res.status(201).json({
+            message: `nice to have you, ${result.username}`,
+        })
+    } catch (err) {
+        next(err)
+    }
 })
+
 router.post('/login', async (req, res, next) => {
-    res.json({ message: 'login working' })
+    try {
+        const { username, password } = req.body
+        const [user] = await User.findBy({ username })
+        if (user && bcryptjs.compareSync(password, user.password)) {
+            req.session.user = user
+            res.json({ message: `welcome back, ${user.username}` })
+        } else {
+            next({ status: 401, message: 'bad credentials' })
+        }
+    } catch (err) {
+        next(err)
+    }
 })
+
 router.get('/logout', async (req, res, next) => {
     res.json({ message: 'logout working' })
 })
--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const session = require('express-session')
 const authRouter = require('./auth/auth-router')
 const usersRouter = require('./users/users-router.js')
 
@@ -7,12 +8,25 @@ const server = express()
 
 server.use(express.static(path.join(__dirname, '../client')))
 server.use(express.json())
+server.use(session({
+  name: 'monkey',
+  secret: 'keep it secret',
+  cookie: {
+    maxAge: 1000 * 60 * 60,
+    secure: false,
+    httpOnly: false,
+  },
+  rolling: true,
+  resave: false,
+  saveUninitialized: false,
+}))
+
 server.use('/api/auth', authRouter)
 server.use('/api/users', usersRouter)
 
 server.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client', 'index.html'))
-}) 
+})
 
 server.use('*', (req, res, next) => {
   next({ status: 404, message: 'not found!' })
